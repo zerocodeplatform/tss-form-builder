3f448802add7fd2af41dd293712605dd2ae1117e
Aparanj Venkat <aparanjvenkat.g@thresholdsoft.com>
2018-05-25 12:38:51 +0530

    Version changes to 0.0.5

--- projects/tssforms/src/tss-form/tss-form.component.ts
+++ projects/tssforms/src/tss-form/tss-form.component.ts
@@ -19,77 +19,83 @@ export class TssFormComponent implements OnInit {
       awesomeIsForced: false,
     },
   };
-  fields: FormlyFieldConfig[] = [];
-  //  [
-  //   {
-  //     key: 'text',
-  //     type: 'input',
-  //     templateOptions: {
-  //       label: 'Text',
-  //       placeholder: 'Formly is terrific!',
-  //       required: true,
-  //     },
-  //   },
-  //   {
-  //     key: 'nested.story',
-  //     type: 'textarea',
-  //     templateOptions: {
-  //       label: 'Some sweet story',
-  //       placeholder: 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
-  //       description: '',
-  //     },
-  //     expressionProperties: {
-  //       'templateOptions.focus': 'formState.awesomeIsForced',
-  //       'templateOptions.description': (model, formState) => {
-  //         if (formState.awesomeIsForced) {
-  //           return 'And look! This field magically got focus!';
-  //         }
-  //       },
-  //     },
-  //   },
-  //   {
-  //     key: 'awesome',
-  //     type: 'checkbox',
-  //     templateOptions: { label: '' },
-  //     expressionProperties: {
-  //       'templateOptions.disabled': 'formState.awesomeIsForced',
-  //       'templateOptions.label': (model, formState) => {
-  //         if (formState.awesomeIsForced) {
-  //           return 'Too bad, formly is really awesome...';
-  //         } else {
-  //           return 'Is formly totally awesome? (uncheck this and see what happens)';
-  //         }
-  //       },
-  //     },
-  //   },
-  //   {
-  //     key: 'whyNot',
-  //     type: 'textarea',
-  //     expressionProperties: {
-  //       'templateOptions.placeholder': (model, formState) => {
-  //         if (formState.awesomeIsForced) {
-  //           return 'Too bad... It really is awesome! Wasn\'t that cool?';
-  //         } else {
-  //           return 'Type in here... I dare you';
-  //         }
-  //       },
-  //       'templateOptions.disabled': 'formState.awesomeIsForced',
-  //     },
-  //     hideExpression: 'model.awesome',
-  //     templateOptions: {
-  //       label: 'Why Not?',
-  //       placeholder: 'Type in here... I dare you',
-  //     },
-  //   }
-  // ];
+  fields: FormlyFieldConfig[] = 
+   [
+    {
+      key: 'text',
+      type: 'input',
+      templateOptions: {
+        label: 'Text',
+        placeholder: 'Formly is terrific!',
+        required: true,
+      },
+    },
+    {
+      key: 'nested.story',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Some sweet story',
+        placeholder: 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
+        description: '',
+      },
+      expressionProperties: {
+        'templateOptions.focus': 'formState.awesomeIsForced',
+        'templateOptions.description': (model, formState) => {
+          if (formState.awesomeIsForced) {
+            return 'And look! This field magically got focus!';
+          }
+        },
+      },
+    },
+    {
+      key: 'awesome',
+      type: 'checkbox',
+      templateOptions: { label: '' },
+      expressionProperties: {
+        'templateOptions.disabled': 'formState.awesomeIsForced',
+        'templateOptions.label': (model, formState) => {
+          if (formState.awesomeIsForced) {
+            return 'Too bad, formly is really awesome...';
+          } else {
+            return 'Is formly totally awesome? (uncheck this and see what happens)';
+          }
+        },
+      },
+    },
+    {
+      key: 'whyNot',
+      type: 'textarea',
+      expressionProperties: {
+        'templateOptions.placeholder': (model, formState) => {
+          if (formState.awesomeIsForced) {
+            return 'Too bad... It really is awesome! Wasn\'t that cool?';
+          } else {
+            return 'Type in here... I dare you';
+          }
+        },
+        'templateOptions.disabled': 'formState.awesomeIsForced',
+      },
+      hideExpression: 'model.awesome',
+      templateOptions: {
+        label: 'Why Not?',
+        placeholder: 'Type in here... I dare you',
+      },
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
-    this.fields = this.options.fields;
+    // this.fields = this.options.fields;
   }
 
-  submit() {
+/**
+ * On submit button click the form will be validated.
+ * Form will be redirected to the 
+ * 
+ * @memberof TssFormComponent
+ */
+submit() {
     if (this.form.valid) {
     //  this.router.navigate(['/page/' + this.options.navigateTo]);
     }
