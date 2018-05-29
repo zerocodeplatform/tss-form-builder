09352004ba29a00b4ca97c160b112230e7e3c8e2
Aparanj Venkat <aparanjvenkat.g@thresholdsoft.com>
2018-05-29 16:52:37 +0530

    Version 0.0.6

--- projects/tssforms/src/tss-form/tss-form.component.ts
+++ projects/tssforms/src/tss-form/tss-form.component.ts
@@ -21,70 +21,70 @@ export class TssFormComponent implements OnInit {
       awesomeIsForced: false,
     },
   };
-  fields: FormlyFieldConfig[] = 
+  fields: FormlyFieldConfig[] = [];
 
-   [
-    {
-      key: 'text',
-      type: 'input',
-      templateOptions: {
-        label: 'Text',
-        placeholder: 'Formly is terrific!',
-        required: true,
-      },
-    },
-    {
-      key: 'nested.story',
-      type: 'textarea',
-      templateOptions: {
-        label: 'Some sweet story',
-        placeholder: 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
-        description: '',
-      },
-      expressionProperties: {
-        'templateOptions.focus': 'formState.awesomeIsForced',
-        'templateOptions.description': (model, formState) => {
-          if (formState.awesomeIsForced) {
-            return 'And look! This field magically got focus!';
-          }
-        },
-      },
-    },
-    {
-      key: 'awesome',
-      type: 'checkbox',
-      templateOptions: { label: '' },
-      expressionProperties: {
-        'templateOptions.disabled': 'formState.awesomeIsForced',
-        'templateOptions.label': (model, formState) => {
-          if (formState.awesomeIsForced) {
-            return 'Too bad, formly is really awesome...';
-          } else {
-            return 'Is formly totally awesome? (uncheck this and see what happens)';
-          }
-        },
-      },
-    },
-    {
-      key: 'whyNot',
-      type: 'textarea',
-      expressionProperties: {
-        'templateOptions.placeholder': (model, formState) => {
-          if (formState.awesomeIsForced) {
-            return 'Too bad... It really is awesome! Wasn\'t that cool?';
-          } else {
-            return 'Type in here... I dare you';
-          }
-        },
-        'templateOptions.disabled': 'formState.awesomeIsForced',
-      },
-      hideExpression: 'model.awesome',
-      templateOptions: {
-        label: 'Why Not?',
-        placeholder: 'Type in here... I dare you',
-      },
-    }
-  ];
+  //  [
+  //   {
+  //     key: 'text',
+  //     type: 'input',
+  //     templateOptions: {
+  //       label: 'Text',
+  //       placeholder: 'Formly is terrific!',
+  //       required: true,
+  //     },
+  //   },
+  //   {
+  //     key: 'nested.story',
+  //     type: 'textarea',
+  //     templateOptions: {
+  //       label: 'Some sweet story',
+  //       placeholder: 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
+  //       description: '',
+  //     },
+  //     expressionProperties: {
+  //       'templateOptions.focus': 'formState.awesomeIsForced',
+  //       'templateOptions.description': (model, formState) => {
+  //         if (formState.awesomeIsForced) {
+  //           return 'And look! This field magically got focus!';
+  //         }
+  //       },
+  //     },
+  //   },
+  //   {
+  //     key: 'awesome',
+  //     type: 'checkbox',
+  //     templateOptions: { label: '' },
+  //     expressionProperties: {
+  //       'templateOptions.disabled': 'formState.awesomeIsForced',
+  //       'templateOptions.label': (model, formState) => {
+  //         if (formState.awesomeIsForced) {
+  //           return 'Too bad, formly is really awesome...';
+  //         } else {
+  //           return 'Is formly totally awesome? (uncheck this and see what happens)';
+  //         }
+  //       },
+  //     },
+  //   },
+  //   {
+  //     key: 'whyNot',
+  //     type: 'textarea',
+  //     expressionProperties: {
+  //       'templateOptions.placeholder': (model, formState) => {
+  //         if (formState.awesomeIsForced) {
+  //           return 'Too bad... It really is awesome! Wasn\'t that cool?';
+  //         } else {
+  //           return 'Type in here... I dare you';
+  //         }
+  //       },
+  //       'templateOptions.disabled': 'formState.awesomeIsForced',
+  //     },
+  //     hideExpression: 'model.awesome',
+  //     templateOptions: {
+  //       label: 'Why Not?',
+  //       placeholder: 'Type in here... I dare you',
+  //     },
+  //   }
+  // ];
 
   // formProp: FormProperties[];
 
